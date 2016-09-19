bedea7600d76462d74c2cc6e3c2e9db5f915c70b
spectrecode <mchristy.ccb@gmail.com>
2016-09-19 18:48:13 -0500

    agregando boton

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("boton");
+	boton.disabled = true;
 	boton.addEventListener("click", function(e) {
 		e.preventDefault();
+
 		var texto = document.getElementById("txt");
 		var cajaTexto = texto.value;
 		agregarMensaje(cajaTexto);
@@ -13,4 +15,9 @@ window.addEventListener("load", function() {
 		var contenedor = document.getElementById("contenedor");
 	 	contenedor.insertBefore(publicar,contenedor.childNodes[0]).classList.add("caja");
 	}
+	
+	var textArea = document.getElementById("txt");
+	textArea.addEventListener("keydown", function() {
+		boton.disabled = false;
+	}) 
 });
